a4be57f02bd3c433c6832ebe369e6ecf982b6fd4
Stephen Pham <pham.stephen@gmail.com>
2013-09-15 22:18:38 +1000

    Main controller servlet

--- JunkHub/src/edu/unsw/triangle/servlet/Controller.java
+++ JunkHub/src/edu/unsw/triangle/servlet/Controller.java
@@ -7,6 +7,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Main controller  
+ */
 public class Controller extends HttpServlet
 {
 
